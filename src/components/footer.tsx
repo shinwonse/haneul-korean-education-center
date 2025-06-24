fef44d942c1b480bcd0041c69f3795c9ef210ea4
shinwonse <sinwonse@gmail.com>
2025-06-24 11:08:59 +0900

    Remove unused footer link to improve clarity and maintainability

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -30,16 +30,6 @@ export function Footer() {
         >
           <Image src="/blog.png" alt="blog" width={40} height={40} />
         </Link>
-
-        <Link
-          href="https://band.us/n/a9adbfdah9U4D"
-          target="_blank"
-          className={cn(
-            'w-20 h-20 rounded-full bg-[#2A338E] shadow-[2px_3px_10px_rgba(0,0,0,0.51)] flex items-center justify-center',
-          )}
-        >
-          <Image src="/band.png" alt="band" width={60} height={40} />
-        </Link>
       </div>
       <div>
         <p className={cn('text-white text-xs')}>
